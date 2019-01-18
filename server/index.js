342d6f4f06a0ce401a18c24e4bbea2a113b3fdc4
shane <shanejlester@gmail.com>
2019-01-17 21:23:18 -0800

    Change port to use config variable of heroku

--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ const express = require('express');
 const app = express();
 const { db } = require('./models');
 const path = require('path');
+const port = process.env.PORT || 3000;
+
 
 app.use(express.static(path.join(__dirname, '..', 'client', 'public')));
 app.use('/api', require('./api'));
 
 db.sync().then( () => console.log("DB synced!")); 
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('App is listening on port 3000'); 
 });
