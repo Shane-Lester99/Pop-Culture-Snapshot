b177d8b40059339fe5e447c4bcea217d1754678f
shane <shanejlester@gmail.com>
2019-01-17 18:36:49 -0800

    Add data for Heroku

--- server/models/db.js
+++ server/models/db.js
@@ -1,7 +1,14 @@
+
+
 const Sequelize = require('sequelize');
 
+let db;
 
+if (process.env.DATABASE_LOGIN) { 
+    db = new Sequelize(process.env.DATABASE_LOGIN);db = new Sequelize(process.env.DATABASE_LOGIN);
+} else {
+    db = new Sequelize(config.database, config.username, config.password, config)
+}
 
-const db = new Sequelize(process.env.DATABASE_LOGIN);
 
 module.exports = db;
