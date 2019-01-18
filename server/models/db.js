ceccac07c3569ab3ea62aac1af687ddc277228d8
shane <shanejlester@gmail.com>
2019-01-17 21:15:09 -0800

    Try process.env.DATABASE_URL for the db connection

--- server/models/db.js
+++ server/models/db.js
@@ -5,9 +5,10 @@ const Sequelize = require('sequelize');
 let db;
 
 if (process.env.DATABASE_LOGIN) { 
-    db = new Sequelize(process.env.DATABASE_LOGIN);db = new Sequelize(process.env.DATABASE_LOGIN);
+    db = new Sequelize(process.env.DATABASE_LOGIN);
 } else {
-    db = new Sequelize(config.database, config.username, config.password, config)
+    //db = new Sequelize(config.database, config.username, config.password, config)
+    db = new Sequelize(process.env.DATABASE_URL);
 }
 
 
