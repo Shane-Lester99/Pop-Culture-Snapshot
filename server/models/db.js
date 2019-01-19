23c21158b720275ebafefcd009552c150f3abbe1
shane <shanejlester@gmail.com>
2019-01-19 15:29:04 -0800

    Added proper error message and exit if production and developer db undefined

--- server/models/db.js
+++ server/models/db.js
@@ -7,10 +7,13 @@ let db;
 if (process.env.DATABASE_LOGIN) { 
     console.log('USING DEVELOPERS DB');
     db = new Sequelize(process.env.DATABASE_LOGIN);
-} else {
+} else if (process.env.DATABASE_URL) {
     console.log('USING PRODUCTION DB', process.env.DATABASE_URL);
     //db = new Sequelize(config.database, config.username, config.password, config)
     db = new Sequelize(process.env.DATABASE_URL);
+} else {
+    console.error("DB CONFIG ERROR. Neither developer nor production database has a configuration url. Exiting.");
+    process.exit(1);
 }
 
 
