ade6f79d0003021384e8c5ff3badb7ed2510c992
Jimmy <jimmyyuan33@gmail.com>
2019-01-24 04:12:08 -0500

    Style

--- client/components/Youtube.js
+++ client/components/Youtube.js
@@ -10,7 +10,7 @@ class YoutubeComp extends React.Component{
         <img
         src={video.thumbnail}
         onClick={() => this.props.clickBox('youtube',video)}
-        alt="Youtube Pic">
+        alt="default_youtube_icon.svg">
         </img>
       </div>
   );
